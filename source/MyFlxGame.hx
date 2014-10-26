2d4e1951b6ed0e8d471d0adfd8e067658908fd75
Jaxson Van Doorn <woofers1tt@gmail.com>
2014-10-26 12:15:55 -0700

    0.0.0.1

--- source/MyFlxGame.hx
+++ source/MyFlxGame.hx
@@ -14,26 +14,25 @@ class MyFlxGame extends FlxGame
 	override private function onFocus(_):Void
 	{
 		#if flash
-		if (!_lostFocus) 
+		if (!_lostFocus)
 		{
-			return; // Don't run this function twice (bug in standalone flash player)
+			return;
 		}
 		#end
-		
+
 		#if desktop
-		
-		// make sure the on focus event doesn't fire on startup 
+
 		if (!_onFocusFiredOnce)
 		{
 			_onFocusFiredOnce = true;
 			return;
 		}
 		#end
-		
+
 		_lostFocus = false;
 		FlxG.signals.focusGained.dispatch();
-		
-		if (!FlxG.autoPause) 
+
+		if (!FlxG.autoPause)
 		{
 			_state.onFocus();
 			return;
@@ -42,37 +41,37 @@ class MyFlxGame extends FlxGame
 		#if !FLX_NO_DEBUG
 		debugger.stats.onFocus();
 		#end
-		
+
 		stage.frameRate = FlxG.drawFramerate;
 		#if !FLX_NO_SOUND_SYSTEM
 		FlxG.sound.onFocus();
 		#end
 		FlxG.inputs.onFocus();
 	}
-	
+
 	override private function onFocusLost(_):Void
 	{
 		#if flash
-		if (_lostFocus) 
+		if (_lostFocus)
 		{
-			return; // Don't run this function twice (bug in standalone flash player)
+			return;
 		}
 		#end
-		
+
 		_lostFocus = true;
-		
+
 		FlxG.signals.focusLost.dispatch();
-		
-		if (!FlxG.autoPause) 
+
+		if (!FlxG.autoPause)
 		{
 			_state.onFocusLost();
 			return;
 		}
-		
+
 		#if !FLX_NO_DEBUG
 		debugger.stats.onFocusLost();
 		#end
-		
+
 		stage.frameRate = focusLostFramerate;
 		#if !FLX_NO_SOUND_SYSTEM
 		FlxG.sound.onFocusLost();
