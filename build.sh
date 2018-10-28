bd6ff055454dcc6cd38a425db72f1d1f3d73fad9
Jaxson Van Doorn <woofers1tt@gmail.com>
2018-10-28 14:12:28 -0700

    Apply custom CSS during build
    Prior to this commit the `index.html` was simply
    copied into the build to apply custom CSS styles.
    However this caused an issue where newer versions
    of HaxeFlixel would invoke the game using
    a differently Javascript method.
    
    To prevent these updates from breaking the game,
    the CSS styles are simply added into the `head`
    of the HTML file after the build.

--- build.sh
+++ build.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
+
+# Setup
+set -o noclobber
+OUT=export/html5/bin
+INDEX=$OUT/index.html
+TEMP=$OUT/temp.html
+
+# Build game
+rm -rf $OUT >> /dev/null
 haxelib run openfl build Project.xml html5
-cp index.html export/html5/bin/
+
+# Apply custom CSS
+cp $INDEX $TEMP
+CSS="<style> #openfl-content { background: transparent !important; } <\/style>"
+CSS=$CSS"<style> canvas { border-radius: 20px; } <\/style>"
+sed "15s/$/ $CSS/" $TEMP >| $INDEX
+rm $TEMP
