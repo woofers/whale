87e921f3c5e79fd805105ceddd33ff03c24acd8e
Jaxson Van Doorn <woofers1tt@gmail.com>
2019-03-24 17:35:19 -0700

    Build game using Node haxelib wrapper

--- build.sh
+++ build.sh
@@ -6,10 +6,6 @@ OUT=export/html5/bin
 INDEX=$OUT/index.html
 TEMP=$OUT/temp.html
 
-# Build game
-rm -rf $OUT >> /dev/null
-haxelib run openfl build Project.xml html5
-
 # Apply custom CSS
 cp $INDEX $TEMP
 CSS="<style> #openfl-content { background: transparent !important; } <\/style>"
